b46640ae0c07e1ac4709ed7f24bf8a38b7fc5265
DanDeLuna <danieldeluna4@gmail.com>
2023-04-20 23:28:53 -0600

    added questions and generatemarkdown file.

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,42 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(licenses){'[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+  }else {return '';}}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) 
+{if(licenses){'[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+}else {return '';}}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(licenses){'[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+  }else {return '';}}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+##Short description 
+##Table of contents
+*Install
+*Usage
+*Test
+*Credits
+*Licenese
+##Install
+${data.install}
+##Usage
+${data.usage}
+##Test
+${data.testts}
+##Credits
+${data.credits}
+##Licenses
+${data.licenses}
 `;
-}
+} 
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -25,7 +26,7 @@ const questions = [
     },
     {
     type: 'input',
-    name: 'test',
+    name: 'testts',
     message:'What tests have been performed?',
     },
     {
@@ -35,21 +36,41 @@ const questions = [
     },
     {
     type: 'input',
-    name: 'license',
-    message:'',
+    name: 'licenses',
+    message:'What License did you use?',
     },
     {
     type:'input',
     name:'question',
-    message:'',
+    message:'Do you have any questions?',
+    },
+    {
+     type:'input',
+    name:'linkedin',
+    message:'What is your linkedin?',
+    },
+    {
+    type:'input',
+    name:'github',
+    message:'Link your github',
+    },
+    {
+    type:'input',
+    name:'email',
+    message:'What is your email?',
     },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data)),(err) => err ? console.log(err) : 
+    console.log('You just  created an awesome readme.md!')
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.createprompt(questions).then((data)=> writeToFile('readme.md',data))
+}
 
 // Function call to initialize app
 init();
